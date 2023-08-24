3bff3786ae084e979114dc6127fa9e516acdbea3
michallef <142462683+michallef@users.noreply.github.com>
2023-08-24 15:46:11 -0700

    Create main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 def main():
-    """
-    ##################################################
-    # Comlete your code here
-    Use the same variables: celcius fahrenheit 
-    ##################################################
-    """
 
-    """
+    celcius = int(input("Enter the temperature in celcius: "))
+    fahrenheit = 9/5 * (celcius) + 32
+    print (f'Fahrenheit: \t {fahrenheit:.2f}')
+
+    # Comlete your code here
     ########################################
     # Do not delete the return statement
     ########################################
-    """
     return celcius, fahrenheit
 
 
